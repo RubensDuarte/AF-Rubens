fbe7d79bf4f089a1cdf681d42741bfc4ceed0dca
RubensDuarte <144288751+RubensDuarte@users.noreply.github.com>
2023-12-24 20:36:46 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+// Adicione rotas personalizadas aqui, se necessário
+
+server.use(router);
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
